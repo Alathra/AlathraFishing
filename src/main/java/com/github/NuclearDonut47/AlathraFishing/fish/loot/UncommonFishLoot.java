92e7ffec4a6a805d2f65fd8d6043ff91edb05e5e
George J. Economou II <35174543+NuclearDonut47@users.noreply.github.com>
2023-09-12 20:37:18 -0400

    Framework

---
+++ src/main/java/com/github/NuclearDonut47/AlathraFishing/fish/loot/UncommonFishLoot.java
@@ -0,0 +1,14 @@
+package com.github.NuclearDonut47.AlathraFishing.fish.loot;
+
+import com.github.NuclearDonut47.AlathraFishing.fish.util.LootTable;
+import org.bukkit.inventory.ItemStack;
+
+public class UncommonFishLoot implements LootTable {
+    // Handles generating loot for uncommon catches via angling.
+    // Any logic concerning logic for what can and cannot be caught as an uncommmon angling reward, when, goes here.
+
+    @Override
+    public ItemStack generateLoot() {
+        return null;
+    }
+}
